6feb82ad9597322665974c621574fb4ae5dc6f0e
marchoeppner <mphoeppner@gmail.com>
2019-02-14 08:39:56 +0100

    Parallelized EST steps

--- bin/runExonerate_fromBlastHits_prot2genome.pl
+++ bin/runExonerate_fromBlastHits_prot2genome.pl
@@ -4,7 +4,6 @@
 
 use strict;
 use warnings;
-use Bio::SeqIO;
 
 my $num_args               = $#ARGV + 1;
 if ($num_args != 3)
@@ -19,57 +18,15 @@ if ($num_args != 3)
 }
 
 my $blast_output_parsed = $ARGV[0];	# FILE WITH UNIQUE QUERY - SCAFFOLD COMBINATIONS
-my $queries_fasta = $ARGV[1];		# FASTA FILE WITH ALL QUERIES
-my $genome_file = $ARGV[2];		# GENOME FILE
-my $genome;							# GENOME FILE BASENAME
+my $queries_fasta = $ARGV[1];		# FASTA FILE CDBTOOLS INDEX
+my $genome_basename = $ARGV[2];		# PATH TO GENOME SEQUENCES
 my $line;
 my @temp;
 my $query;							# PROTEIN QUERY OF THAT HIT
 my $target;							# TARGET SCAFFOLD OF THAT HIT
-my $scaffold;						# THAT SCAFFOLD NAME
-my $seqs;							# THAT SCAFFOLD SEQUENCE
-my $genome_basename;
-my $seqio_obj;						# SEQIO OBJECT WITH ALL QUERIES
-my $seq_obj;						# SEQIO SEQUENCE OBJECT WITH THAT QUERY
-my $seqout;						# SEQIO OUTFILE OBJECT
-
-
-open(GENOME, "<$genome_file") || die "ERROR: cannot open $genome_file";
-
-$genome_basename = $genome_file;
-$genome_basename =~ s/\..*$//;
-
-# CREATE DIRECTORY WITH ONE FILE PER SCAFFOLD
-if (-e $genome_basename and -d $genome_basename) {
-
-} else {
-	system("mkdir $genome_basename");
-
-	while (<GENOME>) {
-		$line = $_;
-		chomp $line;
-		
-    	# WRITE THE HEADER:
-   		if ($line=~/^>/){
-			($scaffold) = ($line=~ /^>(\S+)/);
-			open (SCFILE, ">$genome_basename/$scaffold.fasta");
-			print STDOUT "Created file $scaffold.fasta in directory $genome_basename\n";
-			print SCFILE ">$scaffold\n";
-			next;
-			
-		# WRITE THE SEQUENCE:
-		} else {
-			$seqs = $line;
-			print SCFILE "$seqs\n";
-		}
-		next;
-	}
-	close GENOME;
-}
 
 # RUN EXONERATE FOR EACH PROTEIN-SCAFFOLD MATCH:
 open(MATCHES, "<$blast_output_parsed") || die "ERROR: cannot open $blast_output_parsed";
-
 
 # READ EACH HIT ENTRY: 
 while (<MATCHES>){
@@ -82,10 +39,10 @@ while (<MATCHES>){
 	my $query_clean = $query;
 	$query_clean =~ s/\|/_/g ; # Strip characters that will break bash
 	my $fa_file = "$query_clean.fa" ;
-	
-	#printf "cdbyank $queries_fasta -a '$query' > $fa_file \n"; 
-	
-	system("cdbyank $queries_fasta -a '$query' > $fa_file");
+
+	unless (-e $fa_file) {	
+		system("cdbyank $queries_fasta -a '$query' > $fa_file");
+	}
 
 	printf "exonerate --model protein2genome --softmaskquery yes --softmasktarget yes --bestn 1 --minintron 20 --maxintron 20000  --showalignment false --showtargetgff true $fa_file $genome_basename/$target.fasta >> $query_clean.exonerate.out\n";
 }
